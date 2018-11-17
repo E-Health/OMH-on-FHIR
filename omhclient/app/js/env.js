989966cc3f519017428f57bcd542a29afe070c4c
dermatologist <github@gulfdoctor.net>
2018-11-16 20:50:43 -0500

    small change in env

--- omhclient/app/js/env.js
+++ omhclient/app/js/env.js
@@ -7,6 +7,6 @@
     window.__env.omhOnFhirClientId = '93651a15-4664-486e-8661-eca7ebc21bda';
     window.__env.omhOnFhirScope = 'patient/*.read launch'; //to force provider login use the following scopes 'openid profile'
     window.__env.omhOnFhirRedirectUri = 'http://tomcat.nuchange.ca/omhonfhir/login';
-    window.__env.omhOnFhirAPIBase = 'https://tomcat.nuchange.ca/mdata';
+    window.__env.omhOnFhirAPIBase = 'http://tomcat.nuchange.ca/mdata';
     window.__env.omhOnFhirAPIShimmerAuth = '/authorize';
 }(this));
