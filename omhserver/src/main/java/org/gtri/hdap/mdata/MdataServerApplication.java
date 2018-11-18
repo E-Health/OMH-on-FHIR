59122fbdab4d5c8b6519020ad3bd14faa85c9f16
dermatologist <github@gulfdoctor.net>
2018-11-18 18:28:51 -0500

    Drishti branch. mvn clean install as maven dependency for drishti-cog

--- omhserver/src/main/java/org/gtri/hdap/mdata/MdataServerApplication.java
+++ omhserver/src/main/java/org/gtri/hdap/mdata/MdataServerApplication.java
@@ -2,13 +2,18 @@ package org.gtri.hdap.mdata;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class,
+		HibernateJpaAutoConfiguration.class,
+		DataSourceTransactionManagerAutoConfiguration.class
+})
+@ComponentScan
 public class MdataServerApplication {
 
 	public static void main(String[] args) {
