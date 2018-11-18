403cae3fe1bd2a26047bc7073682fb7005959143
dermatologist <github@gulfdoctor.net>
2018-11-18 09:09:52 -0500

    Datasource autoconfig removed.

--- omhserver/src/main/java/org/gtri/hdap/mdata/MdataServerApplication.java
+++ omhserver/src/main/java/org/gtri/hdap/mdata/MdataServerApplication.java
@@ -3,7 +3,7 @@ package org.gtri.hdap.mdata;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class MdataServerApplication {
 
 	public static void main(String[] args) {
