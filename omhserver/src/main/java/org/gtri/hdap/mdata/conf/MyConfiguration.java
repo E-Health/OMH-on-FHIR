4ceed355d7d1655f5064f21e6498fc3d7b4032d8
dermatologist <github@gulfdoctor.net>
2018-11-18 18:21:12 -0500

    Before refactor

--- omhserver/src/main/java/org/gtri/hdap/mdata/conf/MyConfiguration.java
+++ omhserver/src/main/java/org/gtri/hdap/mdata/conf/MyConfiguration.java
@@ -1,17 +1,15 @@
-package org.gtri.hdap.mdata;
+package org.gtri.hdap.mdata.conf;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-public class MdataServerApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(MdataServerApplication.class, args);
-	}
+@Configuration
+@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class,
+        DataSourceTransactionManagerAutoConfiguration.class
+})
+public class MyConfiguration {
 }
